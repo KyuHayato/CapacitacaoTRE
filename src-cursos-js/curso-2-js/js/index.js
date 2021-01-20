7671b00a1c03b2a294408e9f1c5c3b7af91532ec
Ewerson Alves <ewerson.alves@live.com>
2021-01-20 14:05:30 -0300

    Atualização e continuação do segundo curso

--- src-cursos-js/curso-2-js/js/index.js
+++ src-cursos-js/curso-2-js/js/index.js
@@ -1,17 +1,37 @@
-	let titulo = document.querySelector(".titulo");
-    titulo.textContent = "Estudos dirigidos de JavaScript";
-    
-    let paciente = document.querySelector("#primeiro-paciente");
-    
+let titulo = document.querySelector(".titulo");
+titulo.textContent = "Estudos dirigidos de JavaScript";
+
+let pacientes = document.querySelectorAll(".paciente");
+
+for (let i = 0; i < pacientes.length; i++) {
+    console.log(pacientes[i])
+    let paciente = pacientes[i];
     let tdPeso = paciente.querySelector(".info-peso");
     let peso = tdPeso.textContent;
-
     let tdAltura = paciente.querySelector(".info-altura");
-    let altura  = tdAltura.textContent;
-
-    let imc = peso / (altura * altura)
-    
+    let altura = tdAltura.textContent;
+    let imc;
     let tdImc = paciente.querySelector(".info-imc");
 
-    tdImc.textContent = imc;
-    
+    if (peso <= 200 && peso > 1 && (altura < 2.30) && (altura > 0.1)) {
+        imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+
+    } else {
+        if ((peso >= 200) || (peso < 1)) {
+            tdPeso.textContent = "peso invalido";
+        }
+        if ((altura > 2.30) || (altura < 0.1)) {
+            tdAltura.textContent = "altura invalida";
+        }
+        console.log("dados invalidos")
+        alert("dados invalidos")
+        tdImc.textContent = "dados invalidos";
+    }
+}
+
+
+
+
+
+
