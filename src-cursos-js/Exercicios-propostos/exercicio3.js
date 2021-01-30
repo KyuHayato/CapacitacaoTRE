734bad28772f4d6d5cd5f07583b86be07fe84819
Ewerson Alves <ewerson.alves@live.com>
2021-01-30 16:17:05 -0300

    Finalização do  2º curso

--- src-cursos-js/Exercicios-propostos/exercicio3.js
+++ src-cursos-js/Exercicios-propostos/exercicio3.js
@@ -5,47 +5,19 @@ Qual é o maior fator primo do número 600851475143 ? */
 precisar de uma função para saber se é primo e usar ela num loop
 
 Pesquisa crivo de erastotenes para descobrir se é primo. É o algoritmo básico */
-// Gambiarra sem sucesso 
-let valorLimite = 10;
-let x = 2
-while (x <= valorLimite) {
-    if (valorLimite % 2 == 0) {
-        x = 3;
-    } else {
-        console.log(valorLimite);
-        valorLimite--;
+
+
+function ehPrimo(numero) {
+    for (let divisor = 2; divisor <= Math.sqrt(numero); divisor++) {
+        if (numero % divisor === 0) return false;
     }
-    if ((valorLimite) % 3 == 0) {
-        x = 5;
-    } else {
-        console.log(valorLimite);
-        valorLimite--;
-    }
-    if ((valorLimite) % 5 == 0) {
-        x = 7;
-    } else {
-        console.log(valorLimite);
-        valorLimite--;
-    
-    } if ((valorLimite) % 7 == 0) {
-        x = 11;
-    } else {
-        console.log(valorLimite);
-        valorLimite--;
-    }
-    if((valorLimite) % 11 == 0){
-        x = 13;
-    } else {
-        console.log(valorLimite);
-        valorLimite--;
-    } 
-    if((valorLimite) % 11 == 0){
-        x = 17;
-    } else {
-        console.log(valorLimite);
-        valorLimite--;
-    }
-    console.log(valorLimite);
-    x++;
+    return true;
 }
 
+function maiorFatorPrimo(numero) {
+    for (let divisor = Math.floor(Math.sqrt(numero)); divisor >= 2; divisor--)
+        if (numero % divisor === 0 && ehPrimo(divisor)) return divisor;
+    return 1;
+}
+
+console.log(maiorFatorPrimo(600851475143))
--- src-cursos-js/curso-2-js/js/buscar-pacientes.js
+++ src-cursos-js/curso-2-js/js/buscar-pacientes.js
@@ -3,17 +3,26 @@ var botaoAdicionar = document.querySelector("#buscar-pacientes");
 botaoAdicionar.addEventListener('click', function () {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-    xhr.addEventListener("load", function () {
-        var resposta = xhr.responseText;
-        console.log(resposta)
-        console.log(typeof resposta)
-        var pacientes = JSON.parse(resposta);
-        console.log(pacientes)
-        console.log(typeof pacientes)
 
+    xhr.addEventListener("load", function () {
+    var erroAjax = document.querySelector("#erro-ajax");
+        if(xhr.status == 200){
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+        var pacientes = JSON.parse(resposta);
+        
         pacientes.forEach(function(paciente) {
             adicionaPacienteNaTabela(paciente);
+            console.log(xhr.status)
         });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText)
+            var erroAjax = document.querySelector("#erro-ajax")
+            erroAjax.classList.remove("invisivel")
+
+        }
+        
     })
     xhr.send();
 })
