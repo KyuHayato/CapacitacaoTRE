be457a9c75591e14c42ddaf6f36c9c4874907670
Leonardo Galvão <leonardo.galvao@gmail.com>
2021-02-02 12:36:23 -0300

    Update teste-exercicio4.js

--- src-cursos-js/Exercicios-propostos/teste-exercicio4.js
+++ src-cursos-js/Exercicios-propostos/teste-exercicio4.js
@@ -1,32 +1,38 @@
 function verificaMaiorPalindromo() {
     let maiorPalindromoEncontrar = false;
 
-    for (var valorE = 999; valorE >= 0; valorE--) {
-        for (var valorD = 999; valorD >= 0; valorD--) {
+    // Esses loops não garantem pegar o máximo
+    for (let valorE = 999; valorE >= 0; valorE--) {
+        for (let valorD = 999; valorD >= 0; valorD--) {
             let resultado = valorE * valorD;
 
-            if (String(resultado.length) % 2 == 0) {
+            // Por que essa verificação se é par?
+            if (String(resultado.length) % 2 === 0) {
                 let ladoA = String(resultado.slice(0, Number(String(resultado.length)) / 2));
-                
-                let ladoB = String(resultado.slice(String(resultado.lastIndexOf)), Number(String(resultado.length)) / 2)
-                
-                let ladoBInvertido = '';
-                for (var num = String(ladoB.length); num >= 0; num--) { 
-                     ladoBInvertido = ladoB[num]++;
+                // Lastindexof está sendo passado sem parâmetro!
+                let ladoB = String(
+                    resultado.slice(String(resultado.lastIndexOf)),
+                    Number(String(resultado.length)) / 2
+                )
 
-                    if (ladoA == ladoBInvertido) { 
+                // Sugiro criar uma função separada que verifica se é palíndromo.
+                let ladoBInvertido;
+                for (let num = String(ladoB.length); num >= 0; num--) {
+                    ladoBInvertido = ladoB[num]++;
+                    if (ladoA === ladoBInvertido) {
                         console.log(valorE + "X" + valorD + "=" + resultado + "é palíndromo");
                         console.log("Lado A: " + ladoA + ", Lado B:" + ladoB + "\n")
                         maiorPalindromoEncontrar = true;
                     }
                 }
             }
+            // Muito estranho esse if! Melhor fazer a função retornar o resultado.
             if (maiorPalindromoEncontrar) {
                 break;
             }
-
         }
     }
-
 }
+
+// Código roda mas não imprime nada
 verificaMaiorPalindromo()
