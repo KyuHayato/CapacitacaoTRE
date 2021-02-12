8f255174fa3a4b4bda3cbfe912c00f557834f5b9
Ewerson Alves <ewerson.tre@gmail.com>
2021-02-12 12:53:24 -0300

    atualização do curso e continuação dos exercícios faltantes

--- src-cursos-js/Exercicios-propostos/ExerciciosDOM/exercicioDOM-6/main.js
+++ src-cursos-js/Exercicios-propostos/ExerciciosDOM/exercicioDOM-6/main.js
@@ -1,17 +1,38 @@
-let linhas = 0;
-let colunas = 0;
+let linhas;
+let colunas;
 function enviarDados() {
 
    linhas = Number(document.querySelector('#linhas').value)
    colunas = Number(document.querySelector('#colunas').value)
-   return linhas, colunas
-}
-
-function criarTabela(){
-
+   return this.linhas, this.colunas
 }
 
 
+function criarTabela() {
+   const linhas = 3
+   const colunas = 3
+  
+
+
+   for (let i = 0; i < linhas; i++) {
+      for (let j = 0; j < colunas; j++) {
+         const Tr = document.createElement('tr');
+         let conteudo = `<td>Linha${i+=1} Coluna${j+=1}</td> <td>Linha${i} Coluna${j+=1}</td>`;
+         Tr.innerHTML = conteudo;
+         document.querySelector('.celula').appendChild(Tr);
+
+
+         /*          linhaConteudo[i] = document.createElement('td')
+         linhaConteudo[i].textContent = `Linha ${i}`;
+         colunasConteudo[j] = document.createElement('td')
+         colunasConteudo[j].textContent = `Coluna ${j}`;
+         document.querySelector('.celula').appendChild(linhaConteudo)
+         document.querySelector('.celula').appendChild(colunasConteudo)
+ */      }
+     
+   }
+}
+//Não estou conseguindo implementar a lógica de duplicar colunas e de resgatar os valores de linhas e colunas da primeira função
 
 
 
