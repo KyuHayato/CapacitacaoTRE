2cfe296ab556dd5a20fd2d3400acfb606ad2f767
Ewerson Alves <ewerson.tre@gmail.com>
2021-02-19 13:02:35 -0300

    Novo curso de HTML e JS

---
+++ src-cursos-js/curso-5-js/projeto/service/cliente-service.js
@@ -0,0 +1,9 @@
+const http = new XMLHttpRequest()
+http.open('GET', 'http://localhost:3000/profile')
+
+http.send()
+
+http.onload = () => {
+    const data = http.response
+    console.log(data)
+}
