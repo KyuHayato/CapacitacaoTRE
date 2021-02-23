deaeac0f7c94f77369b84273c59e6e79651e955f
Ewerson Alves <ewerson.tre@gmail.com>
2021-02-23 13:12:15 -0300

    Continuação do 5 curso de JS (CRUD)

--- src-cursos-js/curso-5-js/projeto/service/cliente-service.js
+++ src-cursos-js/curso-5-js/projeto/service/cliente-service.js
@@ -1,9 +1,10 @@
-const http = new XMLHttpRequest()
-http.open('GET', 'http://localhost:3000/profile')
+const listaClientes = () => {
+    return fetch(`http://localhost:3000/profile`)
+        .then(resposta => {
+            return resposta.json()
+        })
+}
 
-http.send()
-
-http.onload = () => {
-    const data = http.response
-    console.log(data)
+export const clienteService = {
+    listaClientes
 }
