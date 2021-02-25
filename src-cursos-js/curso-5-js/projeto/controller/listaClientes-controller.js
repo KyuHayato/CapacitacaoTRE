7ac8238283e17028d9f1f7097feed96448f3bd5f
Ewerson Alves <ewerson.tre@gmail.com>
2021-02-25 12:08:12 -0300

    finalização do 5 curso de JS(CRUD)

--- src-cursos-js/curso-5-js/projeto/controller/listaClientes-controller.js
+++ src-cursos-js/curso-5-js/projeto/controller/listaClientes-controller.js
@@ -6,7 +6,7 @@ const criaNovaLinha = (nome, email, id) => {
                 <td>${email}</td>
                 <td>
                     <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" 
+                        <li><a href="../telas/edita_cliente.html?id=${id}" 
                         class="botao-simples botao-simples--editar">Editar</a></li>
                         <li><button class="botao-simples botao-simples--excluir" 
                         type="button">Excluir</button></li>
@@ -20,21 +20,33 @@ const criaNovaLinha = (nome, email, id) => {
 
 const tabela = document.querySelector('[data-tabela]')
 
-tabela.addEventListener('click', (evento) => {
+tabela.addEventListener('click', async (evento) => {
     let ehBotaoDeletar = evento.target.className === 'botao-simples botao-simples--excluir'
     if (ehBotaoDeletar) {
-        const linhaCliente = evento.target.closest('[data-id]')
-        let id = linhaCliente.dataset.id
-        clienteService.removeCliente(id)
-        .then(() =>{
-            linhaCliente.remove() 
-        })
+        try{
+            const linhaCliente = evento.target.closest('[data-id]')
+            let id = linhaCliente.dataset.id
+            await clienteService.removeCliente(id)
+            linhaCliente.remove()
+        } catch(erro){
+            console.log(erro)
+            window.location.href = '../telas/erro.html'
+        }
     }
 })
+const render = async () => {
+    try{
+        const listaClientes = await clienteService.listaClientes()
 
-clienteService.listaClientes()
-    .then(data => {
-        data.forEach(elemento => {
+        listaClientes.forEach(elemento => {
             tabela.appendChild(criaNovaLinha(elemento.nome, elemento.email, elemento.id))
         })
-    })  
+    }
+    catch(erro){
+        console.log(erro)
+        window.location.href = '../telas/erro.html'
+    }
+
+}
+
+render()
--- src-cursos-js/curso-5-js/projeto/service/cliente-service.js
+++ src-cursos-js/curso-5-js/projeto/service/cliente-service.js
@@ -1,34 +1,76 @@
 const listaClientes = () => {
     return fetch(`http://localhost:3000/profile`)
         .then(resposta => {
-            return resposta.json()
+            if (resposta.ok) {
+                return resposta.json()
+            }
+            throw new Error('Não foi possivel listar os clientes')
         })
 }
-const criaCliente = (nome, email) =>{
-    return fetch(`http://localhost:3000/profile`,{
+const criaCliente = (nome, email) => {
+    return fetch(`http://localhost:3000/profile`, {
         method: 'POST',
         headers: {
-        'Content-Type' : 'application/json'
-    },
-    body: JSON.stringify( {
-        nome: nome,
-        email: email
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+        })
     })
-})
-.then(resposta =>{
-    return resposta.body
-})
+        .then(resposta => {
+            if(resposta.ok){
+                return resposta.body
+            }
+                throw new Error('Não foi possivel criar um cliente')
+        })
 }
 
-const removeCliente = (id) =>{
-    return fetch(`http://localhost:3000/profile/${id}`,{
-    method: 'DELETE'       
-})
+const removeCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'DELETE'
+    }).then(resposta =>{
+        if(!resposta.ok){
+            throw new Error('Não foi possivel remover um cliente')
+        }
+    })
+}
+const detalhaCliente = (id) => {
+    return fetch(`http://localhost:3000/profile/${id}`)
+        .then(resposta => {
+            if(resposta.ok){
+                return resposta.json()
+            }
+            throw new Error('Não foi possivel detalhar os clientes')
+        })
 }
 
+const atualizaCliente = (id, nome, email) => {
+    return fetch(`http://localhost:3000/profile/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json'
+
+        },
+        body: JSON.stringify({
+            nome: nome,
+            email: email
+
+        })
+    })
+        .then(resposta => {
+            if(resposta.ok){
+                return resposta.json()
+            }
+            throw new Error('Não foi possivel atualizar os clientes')
+        })
+
+}
 
 export const clienteService = {
     listaClientes,
     criaCliente,
-    removeCliente
+    removeCliente,
+    detalhaCliente,
+    atualizaCliente
 }
