1c938209a6ab74fb23f87faf9a9348da0ff01bd5
Ewerson Alves <ewerson.tre@gmail.com>
2021-03-05 13:43:04 -0300

    Inicio do curso 7 de JS: conhecendo o browser e padrões de projeto

---
+++ src-cursos-js/curso-7-js/projeto/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,6 @@
+class NegociacaoController{
+   adiciona(event){
+    event.preventDefault();
+    alert('Chamei ação no controller')
+   }
+}
---
+++ src-cursos-js/curso-7-js/projeto/client/js/index.js
@@ -0,0 +1,20 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+console.log(campos)
+var tbody = document.querySelector('table tbody')
+document.querySelector('.form').addEventListener('submit', (event) =>{
+    event.preventDefault();
+    var tr = document.createElement('tr')
+    campos.forEach(function(campo){
+        var td = document.createElement('td')
+        td.textContent = campo.value
+        tr.appendChild(td)
+    })
+    var tdVolume = document.createElement('td')
+    tdVolume.textContent = campos[1].value * campos[2].value
+    tr.appendChild(tdVolume)
+    tbody.appendChild(tr)
+})
