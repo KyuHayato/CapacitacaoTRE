da1df4dac18760ced563a6eab71e8a5152a7dac8
Ewerson Alves <ewerson.tre@gmail.com>
2021-03-10 13:32:39 -0300

    Continuação do curso 7 de JS

--- src-cursos-js/curso-7-js/projeto/client/js/app/controllers/NegociacaoController.js
+++ src-cursos-js/curso-7-js/projeto/client/js/app/controllers/NegociacaoController.js
@@ -4,24 +4,35 @@ class NegociacaoController {
       this._inputData = $('#data')
       this._inputQuantidade = $('#quantidade')
       this._inputValor = $('#valor')
-
+      this._listaNegociacoes = new ListaNegociacoes()
+      this._negociacoesView = new NegociacoesView($('#negociacaesView'))
+      
+      this._negociacoesView.update()
    }
 
    adiciona(event) {
 
       event.preventDefault();
 
-      let negociacao = new Negociacao(
-         DataHelper.textoParaData(this._inputData.value),
-         this._inputQuantidade.value,
-         this._inputValor.value
-      );
-
-      console.log(negociacao)
-      console.log(DataHelper.dataParaTexto(negociacao.data))
+      this._listaNegociacoes.adiciona(this._criaNegociacao())
+      this._limpaFormulario()
+      console.log(this._listaNegociacoes.negociacoes)
 
    }
+   _criaNegociacao() {
 
+      return new Negociacao(
+         DataHelper.textoParaData(this._inputData.value),
+         this._inputQuantidade.value,
+         this._inputValor.value)
+   }
+
+   _limpaFormulario() {
+      this._inputData.value = ''
+      this._inputQuantidade.value = 1
+      this._inputValor.value = 0.0
+
+      this._inputData.focus()
+
+   }
 }
-
-
--- src-cursos-js/curso-7-js/projeto/client/js/app/helpers/DataHelper.js
+++ src-cursos-js/curso-7-js/projeto/client/js/app/helpers/DataHelper.js
@@ -8,6 +8,9 @@ class DataHelper {
     }
 
     static textoParaData(texto) {
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+            throw new Error('Deve estar no formato aaaa-mm-dd')
+        }
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2))
     }
 }
