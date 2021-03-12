1683c7b0f468e7aa457eb6553e6048b24f0795da
Ewerson Alves <ewerson.tre@gmail.com>
2021-03-12 13:11:42 -0300

    inicio do curso 8 de JS

--- src-cursos-js/curso-7-js/projeto/client/js/app/controllers/NegociacaoController.js
+++ src-cursos-js/curso-7-js/projeto/client/js/app/controllers/NegociacaoController.js
@@ -43,7 +43,14 @@ class NegociacaoController {
       this._inputData.focus()
 
    }
-  
+   apaga(){
+      this._listaNegociacoes.esvazia()
+      this._negociacoesView.update(this._listaNegociacoes)
+
+      this._mensagem.texto = 'Negociações apagadas com sucesso!'
+      this._mensagemView.update(this._mensagem)
+      }
+
 } 
 
 
--- src-cursos-js/curso-7-js/projeto/client/js/app/models/ListaNegociacoes.js
+++ src-cursos-js/curso-7-js/projeto/client/js/app/models/ListaNegociacoes.js
@@ -9,5 +9,7 @@ class ListaNegociacoes {
     get negociacoes(){
         return [].concat(this._negociacoes);
     }
-    
+    esvazia(){
+        this._negociacoes = [] 
+     }
 }
