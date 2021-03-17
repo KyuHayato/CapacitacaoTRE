ed7b02c63738c6e2af9a60a9027211895548a1ca
Ewerson Alves <ewerson.tre@gmail.com>
2021-03-17 12:15:23 -0300

    Progresso do curso 8 de JS

--- src-cursos-js/curso-7-e-8-js/projeto/client/js/app/controllers/NegociacaoController.js
+++ src-cursos-js/curso-7-e-8-js/projeto/client/js/app/controllers/NegociacaoController.js
@@ -1,47 +1,33 @@
 class NegociacaoController {
+   //consertar bug
    constructor() {
       let $ = document.querySelector.bind(document);
       this._inputData = $('#data')
       this._inputQuantidade = $('#quantidade')
       this._inputValor = $('#valor')
-      
-      let self = this
-      this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-         get(target, prop, receiver) {
-            if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop] == typeof (Function))) {
-               return function () {
 
-                  console.log(`interceptando ${prop}`)
 
-                  Reflect.apply(target[prop], target, arguments)
-
-                  self._negociacoesView.update(target)
-               }
-            } else {
-
-               return Reflect.get(target, prop, receiver)
-            }
-         }
+      this._listaNegociacoes = ProxyFactory.create(new ListaNegociacoes(), ['adiciona', 'esvazia'], (model) => {
+         this._negociacoesView.update(model)
       })
 
       this._negociacoesView = new NegociacoesView(document.querySelector('#negociacoesView'))
       this._negociacoesView.update(this._listaNegociacoes)
 
-      this._mensagem = new Mensagem()
+      this._mensagem = new ProxyFactory.create(new Mensagem(), ['texto'], (model) => {
+         this._mensagemView.update(model)
+      });
+
       this._mensagemView = new MensagemView(document.querySelector('#mensagemView'))
-      this._mensagemView.update(this._mensagem)
+      this._mensagemView.update(this._mensagem);
 
    }
 
    adiciona(event) {
 
       event.preventDefault();
-
       this._listaNegociacoes.adiciona(this._criaNegociacao())
-
       this._mensagem.texto = 'Negociação adicionada com sucesso!'
-      this._mensagemView.update(this._mensagem)
-
       this._limpaFormulario()
 
    }
@@ -65,7 +51,7 @@ class NegociacaoController {
       this._listaNegociacoes.esvazia()
 
       this._mensagem.texto = 'Negociações apagadas com sucesso!'
-      this._mensagemView.update(this._mensagem)
+
    }
 
 }
---
+++ src-cursos-js/curso-7-e-8-js/projeto/client/js/app/services/ProxyFactory.js
@@ -0,0 +1,34 @@
+class ProxyFactory {
+    //consertar bug
+    static create(objeto, props, acao) {
+
+        return new Proxy(objeto, {
+            get(target, prop, receiver) {
+                if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop]) ) {
+                    return function () {
+
+                        console.log(`interceptando ${prop}`)
+
+                        Reflect.apply(target[prop], target, arguments)
+
+                        return acao(target)
+                    }
+                }
+
+                return Reflect.get(target, prop, receiver)
+
+            },
+             set(target, prop, value, receiver) {
+                if(prop.includes(prop)){
+                    target(prop) = value
+                    acao(target)
+
+                }
+                return Reflect.set(target, prop, value, receiver)
+        }
+        })
+}
+    static _ehFuncao(func){
+       return typeof (func) == typeof (Function)
+    }
+}
