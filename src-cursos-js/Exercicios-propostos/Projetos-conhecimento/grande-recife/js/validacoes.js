ca6d94cb480225eedff19c06c0b8a6bf44ba44ee
Ewerson Alves <ewerson.tre@gmail.com>
2021-05-18 11:12:21 -0300

    validação da pagina de login, refatoração basica e inicio da validação de cadastro

--- src-cursos-js/Exercicios-propostos/Projetos-conhecimento/grande-recife/js/validacoes.js
+++ src-cursos-js/Exercicios-propostos/Projetos-conhecimento/grande-recife/js/validacoes.js
@@ -1,18 +1,45 @@
 function validarLogin() {
-    document.querySelector("#")
-
+    let divCampo1 = document.querySelector("[data-mensagem-erro]")
+    let divCampo2 = document.querySelector("[data-mensagem-erro-senha]")
+    let inputCpfCns = document.querySelector("[data-input-cpf-cns-login]")
+    let inputSenha = document.querySelector("[data-input-password-login]")
+    let mensagemErro = `<span class="text-danger h6 mb-4">Login ou senha inválida</span>`
+    let mensagemErro2 = `<span class="text-danger h6">Login ou senha inválida</span>`
+    
+    if (inputCpfCns.value == "" && inputCpfCns.value != "12345") {
+        divCampo1.innerHTML = mensagemErro  
+    } else{
+        return alert("logando")
+    }
+    if(inputSenha.value == "" && inputSenha.value != "123" ){
+        divCampo2.innerHTML = mensagemErro2
+    }else{
+        return alert("logando")
+    }
 }
 function validarEsqueciSenha() {
-    let mensagemInsira = ` <Span class="text-danger" id="cpf-invalido">Insira um CPF valido</Span>`
-     let mensagemInvalido = `<Span class="text-danger">CPF Invalido ou inexistente</Span>`
-     if (document.querySelector("#botao-cpf-esqueci").value == "") {
-        document.querySelector("#teste").innerHTML = mensagemInsira
+    let mensagemInsira = ` <Span class="text-danger h6" id="cpf-invalido">Insira um CPF valido</Span>`
+    let mensagemInvalido = `<Span class="text-danger h6">CPF Invalido ou inexistente</Span>`
+    if (document.querySelector("#botao-cpf-esqueci").value == "") {
+        document.querySelector("#mensagemInvalida").innerHTML = mensagemInsira
         return false
     }
-    else if(document.querySelector("#botao-cpf-esqueci").value != "12345678"){
-        document.querySelector("#teste").innerHTML = mensagemInvalido
+    else if (document.querySelector("#botao-cpf-esqueci").value != "12345678") {
+        document.querySelector("#mensagemInvalida").innerHTML = mensagemInvalido
         return false
     }
-    
-  return true
+
+    return alert("email enviado")
 }
+
+/* function validacaoTeste(){
+    if(document.querySelector("#teste").style.display == "none"){
+        document.querySelector("#teste").style.display = "block"
+        alert("entrou")
+    } else{
+        document.querySelector("#teste").style.display = "none"
+        alert("else")
+    }
+}
+ */
+
