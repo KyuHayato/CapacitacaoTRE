c6084780578a4476c8ab7c1ac8b86558ca920d14
Ewerson Alves <ewerson.tre@gmail.com>
2021-05-25 11:22:21 -0300

    continuação das validações e pequenos ajustes

--- src-cursos-js/Exercicios-propostos/Projetos-conhecimento/grande-recife/js/validacoes.js
+++ src-cursos-js/Exercicios-propostos/Projetos-conhecimento/grande-recife/js/validacoes.js
@@ -32,55 +32,108 @@ function validarEsqueciSenha() {
     return alert("email enviado")
 }
 
-/* function validacaoTeste() {
-    
+function validacaoTeste() {
+
 
     if (document.querySelector("#input-nome").value == "") {
         document.querySelector("#mensagemErro-nome").style.display = "block"
-        document.querySelector("#input-nome").focus()
+        //document.querySelector("#input-nome").focus()
         
-    } else{
+    } else {
         document.querySelector("#mensagemErro-nome").style.display = "none"
     }
     if (document.querySelector("#input-cpf").value == "") {
         document.querySelector("#mensagemErro-cpf").style.display = "none"
         //document.querySelector("#input-cpf").focus()
-       
-    } else if(document.querySelector("#input-cpf").value != '123.456.789-12'){
-        document.querySelector("#mensagemErro-cpf").style.display = "block"
+        
+    } else if (document.querySelector("#input-cpf").value != '123.456.789-12') {
+        document.querySelector("#mensagemErro-cpf-Nvalido").style.display = "block"
     }
-} */
 
-function ValidaCep(cep) {
-    exp = /\d{2}\.\d{3}\-\d{3}/
-    if (!exp.test(cep.value)) {
-        document.querySelector("#mensagemErro-cep").style.display = "block"
-            cep.focus();
-    }else{
-        document.querySelector("#mensagemErro-cep").style.display = "none"
+    
+    
+}
+
+function validaCPF(cpf) {
+    
+    if (document.querySelector("#input-cpf").value == "") {
+        document.querySelector("#mensagemErro-cpf").style.display = "block"
+        cpf.focus()
+        //document.querySelector("#input-cpf").focus()
+        
+    } else if (document.querySelector("#input-cpf").value != '123.456.789-12') {
+        document.querySelector("#mensagemErro-cpf-Nvalido").style.display = "block"
     }
+    document.querySelector("#mensagemErro-cpf-Nvalido").style.display = "none"
 }
 
 function ValidaTelefone(tel) {
     exp = /\(\d{2}\)\ \d{1}\ \d{4}\-\d{4}/
     if (!exp.test(tel.value)) {
         document.querySelector("#mensagemErro-telefone").style.display = "block"
-            tel.focus();
-    } else{
+        tel.focus();
+    } else {
         document.querySelector("#mensagemErro-telefone").style.display = "none"
     }
 }
 
-function ValidaEmail(email) {
-    exp = /^([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/
-    erroMsg = 'e-Mail Inválido!';
-    if (email.value == '')
-            return false;
-    if (!exp.test(email.value)) {
-            dalert.alert(erroMsg);
-            //email.value='';
-            email.focus();
-            return false;
+function validaDataNasc(){
+    let data = document.querySelector("#data-Nasc").value; // pega o valor do input
+    data = data.replace(/\//g, "-"); // substitui eventuais barras (ex. IE) "/" por hífen "-"
+    let data_array = data.split("-"); // quebra a data em array
+    console.log(data)
+    console.log(data_array)
+
+   
+    let hoje = new Date();
+    console.log(hoje.getFullYear())
+    if(data_array[2] == hoje.getFullYear()){
+        alert("erro")
+    }
+
+}
+function ValidaCep(cep) {
+    exp = /\d{2}\.\d{3}\-\d{3}/
+    if (!exp.test(cep.value)) {
+        document.querySelector("#mensagemErro-cep").style.display = "block"
+        cep.focus();
+    } else {
+        document.querySelector("#mensagemErro-cep").style.display = "none"
     }
 }
+
+
+function ValidaEmail(email) {
+    let exp = /^([0-9a-zA-Z]+[-._+&amp;])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/
+    if ( document.querySelector("#email").value == '') {
+        document.querySelector("#mensagemErro-email-invalido").style.display = "block"
+        email.focus();
+        return false;
+    } else{
+        document.querySelector("#mensagemErro-email-invalido").style.display = "none"
+    }
+    if (!exp.test(email.value)) {
+        
+        document.querySelector("#mensagemErro-email-invalido").style.display = "block"
+        email.focus();
+        return false;
+    }else{
+        document.querySelector("#mensagemErro-email-invalido").style.display = "none"
+    }
+   
+}
+function validacaoCartaoSUS(sus){
+    //ajustar a saida 
+    exp = /\d{3}\''\d{4}\''\d{4}\''\d{4}/ 
+    if(!exp.test(sus.value)){
+        alert('erro')
+    }
+}
+//AJUSTAR validacao
+function validaNum(num){
+    if(document.querySelector("#num") != typeof(Number)){
+        alert(typeof(document.querySelector("#num")))
+    }
+}
+
 
